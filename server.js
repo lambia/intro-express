06655bf5f9670ef463f13146b128c0a37619243b
Luca Lambia <luca@lambia.it>
2026-02-14 09:31:55 +0100

    create express hello world

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 console.log("Benvenuto nel mondo server-side");
+
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+	res.send('Hello World!')
+})
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`)
+})
