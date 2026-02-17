2c0cc11c520d6516ecd9a668f87653d46820134d
Luca Lambia <luca@lambia.it>
2026-02-17 19:34:27 +0100

    api per crud

--- api.js
+++ api.js
@@ -7,12 +7,14 @@ const port = 3000
 
 app.use(express.static('public'));
 
+//Welcome screen
 app.get('/', (req, res) => {
 	console.log("Chiamata ricevuta!");
 	res.send('<html><body><h1>Benvenuto nel server delle pizze</h1><h2>Questa è una web-api, non ci sono altre pagine web.</h2></body></html>')
 })
 
-app.get('/menu/', (req, res) => {
+//Index (cRud)
+app.get('/pizzas', (req, res) => {
 	const menu = [
 		{
 			name: "Margherita",
@@ -40,6 +42,32 @@ app.get('/menu/', (req, res) => {
 	res.json(menu);
 })
 
+//Show (cRud)
+app.get('/pizzas/:id', (req, res) => {
+	res.send(`You requested to SHOW the pizza with id: ${req.params.id}`);
+})
+
+//Store (Crud)
+app.post('/pizzas', (req, res) => {
+	res.send(`You request to CREATE a new pizza`);
+})
+
+//Update (crUd)
+app.put('/pizzas/:id', (req, res) => {
+	res.send(`You requested to UPDATE (complete) the pizza with id: ${req.params.id}`);
+})
+
+//Modify (crUd)
+app.patch('/pizzas/:id', (req, res) => {
+	res.send(`You requested to MODIFY (partial) the pizza with id: ${req.params.id}`);
+})
+
+//Destroy (cruD)
+app.delete('/pizzas/:id', (req, res) => {
+	res.send(`You requested to DELETE the pizza with id: ${req.params.id}`);
+})
+
+//Avvio applicazione
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`)
 })
