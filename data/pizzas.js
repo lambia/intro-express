e877cbeb103d290a0243e541c26d8ce6faac0e0f
Luca Lambia <luca@lambia.it>
2026-02-19 20:22:02 +0100

    aggiunti: index, show e delete

---
+++ data/pizzas.js
@@ -0,0 +1,30 @@
+const menu = [
+	{
+		id: 1,
+		name: "Margherita",
+		image: "/pizze/margherita.webp",
+		ingredients: ["pomodoro", "mozzarella"],
+	}, {
+		id: 2,
+		name: "Marinara",
+		image: "/pizze/marinara.jpeg",
+		ingredients: ["pomodoro", "aglio", "origano"],
+	}, {
+		id: 3,
+		name: "Diavola",
+		image: "/pizze/diavola.jpeg",
+		ingredients: ["pomodoro", "mozzarella", "salame piccante"],
+	}, {
+		id: 4,
+		name: "Bufalina",
+		image: "/pizze/bufalina.jpeg",
+		ingredients: ["pomodoro", "mozzarella di bufala"],
+	}, {
+		id: 5,
+		name: "4 formaggi",
+		image: "/pizze/4_formaggi.jpeg",
+		ingredients: ["pomodoro", "mozzarella", "gorgonzola", "parmigiano", "ricotta"],
+	}
+];
+
+module.exports = menu;
--- routers/pizzas.js
+++ routers/pizzas.js
@@ -1,38 +1,38 @@
 const express = require('express')
 const router = express.Router();
+const menu = require("../data/pizzas");
 
 //Index (cRud)
 router.get('/', (req, res) => {
-	const menu = [
-		{
-			name: "Margherita",
-			image: "/pizze/margherita.webp",
-			ingredients: ["pomodoro", "mozzarella"],
-		}, {
-			name: "Marinara",
-			image: "/pizze/marinara.jpeg",
-			ingredients: ["pomodoro", "aglio", "origano"],
-		}, {
-			name: "Diavola",
-			image: "/pizze/diavola.jpeg",
-			ingredients: ["pomodoro", "mozzarella", "salame piccante"],
-		}, {
-			name: "Bufalina",
-			image: "/pizze/bufalina.jpeg",
-			ingredients: ["pomodoro", "mozzarella di bufala"],
-		}, {
-			name: "4 formaggi",
-			image: "/pizze/4_formaggi.jpeg",
-			ingredients: ["pomodoro", "mozzarella", "gorgonzola", "parmigiano", "ricotta"],
-		}
-	];
 
-	res.json(menu);
+	// console.log(req.query);
+
+	let results = menu;
+
+	//se c'è un filtro => filtra
+	if (req.query.ingredient) {
+		results = menu.filter(pizza => pizza.ingredients.includes(req.query.ingredient));
+	}
+
+	res.json(results);
 })
 
 //Show (cRud)
 router.get('/:id', (req, res) => {
-	res.send(`You requested to SHOW the pizza with id: ${req.params.id}`);
+
+	const id = Number(req.params.id);
+
+	if (isNaN(id)) {
+		return res.status(400).json({ error: "User error", message: "L'id non è valido" });
+	}
+
+	const result = menu.find(pizza => pizza.id == id);
+
+	if (!result) {
+		return res.status(404).json({ error: "Not Found", message: "Pizza non trovata" });
+	}
+
+	return res.json(result);
 })
 
 //Store (Crud)
@@ -52,7 +52,24 @@ router.patch('/:id', (req, res) => {
 
 //Destroy (cruD)
 router.delete('/:id', (req, res) => {
-	res.send(`You requested to DELETE the pizza with id: ${req.params.id}`);
+
+	const id = Number(req.params.id);
+
+	if (isNaN(id)) {
+		return res.status(400).json({ error: "User error", message: "L'id non è valido" });
+	}
+
+	const result = menu.find(pizza => pizza.id == id);
+
+	if (!result) {
+		return res.status(400).json({ error: "Not Found", message: "Pizza non trovata" });
+	}
+
+	menu.splice(menu.indexOf(result), 1);
+
+	console.log(`Pizza:${id} eliminata`, menu);
+
+	return res.sendStatus(204);
 })
 
 module.exports = router;
