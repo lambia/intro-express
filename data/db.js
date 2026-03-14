5e73b876786fc5536b32e118e431a2936f6e34be
Luca Lambia <luca@lambia.it>
2026-03-14 12:07:20 +0100

    aggiunto file .env

--- data/db.js
+++ data/db.js
@@ -1,11 +1,16 @@
 const mysql = require("mysql2");
 
+const dotenv = require("dotenv");
+dotenv.config();
+
+console.log("Variabili env per DB: ",);
+
 const dbConfiguration = {
-	host: "localhost",
-	port: 3306,
-	user: "root",
-	password: "root",
-	database: "pizzeria_db"
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASS,
+	database: process.env.DB_SCHEMA
 }
 
 function onDatabaseConnection(error) {
